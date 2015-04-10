c8035916e9c81f4971fedb8fe46dcb0f13a9512a
derek clark <derekclark14@googlemail.com>
2015-04-10 22:19:34 +0100

    remove unused classes

--- src/main/java/uk/co/socialcalendar/interfaceAdapters/utilities/MailNotification.java
+++
@@ -1,9 +0,0 @@
-//package uk.co.socialcalendar.interfaceAdapters.utilities;
-//
-//public class SendMail implements UserNotification {
-//
-//    @Override
-//    public boolean notify(String recipient, String sender, String message) {
-//        return true;
-//    }
-//}
