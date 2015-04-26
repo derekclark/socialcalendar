5f772e49c11709562d4bd7f8dfa1cb18f48cb6e9
derek clark <derekclark14@googlemail.com>
2015-04-26 14:56:06 +0100

    add AvailabilityHibernate model

---
+++ src/main/java/uk/co/socialcalendar/availability/AvailabilityHibernateModel.java
@@ -0,0 +1,103 @@
+package uk.co.socialcalendar.availability;
+
+import org.joda.time.DateTime;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class AvailabilityHibernateModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="ID")
+    private int id;
+
+    @Column(name="TITLE")
+    private String title;
+
+    @Column(name="START_DATE")
+    private DateTime startDate;
+
+    @Column(name="END_DATE")
+    private DateTime endDate;
+
+    @Column(name="OWNER_EMAIL")
+    private String ownerEmail;
+
+    @Column(name="OWNER_NAME")
+    private String ownerName;
+
+    @Column(name="STATUS")
+    private String status;
+
+    public AvailabilityHibernateModel(){
+
+    }
+
+    public AvailabilityHibernateModel(Availability availability){
+        this.id = availability.getId();
+        this.ownerEmail = availability.getOwnerEmail();
+        this.ownerName = availability.getOwnerName();
+        this.startDate = availability.getStartDate();
+        this.endDate = availability.getEndDate();
+        this.status = availability.getStatus();
+        this.title = availability.getTitle();
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public DateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(DateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/availability/AvailabilityHibernateModelTest.java
@@ -0,0 +1,45 @@
+package availability;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.socialcalendar.availability.Availability;
+import uk.co.socialcalendar.availability.AvailabilityHibernateModel;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AvailabilityHibernateModelTest {
+
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+    public static final String TITLE = "title";
+    public static final String STATUS = "status";
+    public static final DateTime START_DATE = new DateTime();
+    public static final DateTime END_DATE = new DateTime();
+    AvailabilityHibernateModel model;
+    Availability availability;
+
+    @Before
+    public void setup(){
+        model=new AvailabilityHibernateModel();
+        availability = new Availability(EMAIL, NAME, TITLE, START_DATE, END_DATE, STATUS);
+    }
+
+    @Test
+    public void canCreateInstance(){
+        assertTrue(model instanceof AvailabilityHibernateModel);
+    }
+
+    @Test
+    public void canCreateModelFromAvailability(){
+        model = new AvailabilityHibernateModel(availability);
+        assertEquals(EMAIL, model.getOwnerEmail());
+        assertEquals(NAME, model.getOwnerName());
+        assertEquals(TITLE, model.getTitle());
+        assertEquals(START_DATE, model.getStartDate());
+        assertEquals(END_DATE, model.getEndDate());
+        assertEquals(STATUS, model.getStatus());
+    }
+
+}
