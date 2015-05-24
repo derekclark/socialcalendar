f3ad9e73a8e093ae0d03fdccfd89e3bb5a46fb7f
derek clark <derekclark14@googlemail.com>
2015-05-24 01:06:40 +0100

    save availability in database

--- src/main/java/uk/co/socialcalendar/availability/persistence/AvailabilityDAOHibernateImpl.java
+++ src/main/java/uk/co/socialcalendar/availability/persistence/AvailabilityDAOHibernateImpl.java
@@ -1,4 +1,31 @@
 package uk.co.socialcalendar.availability.persistence;
 
-public class AvailabilityDAOHibernateImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import uk.co.socialcalendar.availability.entities.Availability;
+import uk.co.socialcalendar.availability.useCases.AvailabilityFacade;
+
+public class AvailabilityDAOHibernateImpl implements AvailabilityFacade {
+    SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public int create(Availability availability) {
+        Session session = sessionFactory.getCurrentSession();
+        int id = (int) session.save(new AvailabilityHibernateModel());
+        return id;
+    }
+
+    @Override
+    public Availability get(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Availability availability) {
+        return false;
+    }
 }
--- src/test/java/availability/AvailabilityDAOHibernateImplTest.java
+++ src/test/java/availability/AvailabilityDAOHibernateImplTest.java
@@ -1,22 +1,49 @@
 package availability;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
+import uk.co.socialcalendar.availability.entities.Availability;
 import uk.co.socialcalendar.availability.persistence.AvailabilityDAOHibernateImpl;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class AvailabilityDAOHibernateImplTest {
 
     AvailabilityDAOHibernateImpl availabilityDAOImpl;
+    Availability availability;
+    Session mockSession;
+    SessionFactory mockSessionFactory;
 
     @Before
     public void setup(){
         availabilityDAOImpl = new AvailabilityDAOHibernateImpl();
+        availability = new Availability("ownerEmail", "ownerName", "title", new DateTime(), new DateTime(), "status");
+        setupMocks();
+    }
+
+    public void setupMocks(){
+        mockSession = mock(Session.class);
+        mockSessionFactory = mock(SessionFactory.class);
+        availabilityDAOImpl.setSessionFactory(mockSessionFactory);
+        when(mockSessionFactory.getCurrentSession()).thenReturn(mockSession);
     }
 
     @Test
     public void canCreateInstance(){
         assertTrue(availabilityDAOImpl instanceof AvailabilityDAOHibernateImpl);
+    }
+
+    @Test
+    public void canSaveAvailability(){
+        when(mockSession.save(anyObject())).thenReturn(1);
+        availabilityDAOImpl.create(availability);
+        verify(mockSession).save(anyObject());
     }
 }
