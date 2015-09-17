96efa7a0b3e0a81f2366122e6439e1bb4504346e
derek clark <derekclark14@googlemail.com>
2015-09-17 23:19:06 +0100

    adds newsFeed view name

--- src/main/java/uk/co/socialcalendar/newsFeed/NewsFeedController.java
+++ src/main/java/uk/co/socialcalendar/newsFeed/NewsFeedController.java
@@ -1,4 +1,36 @@
 package uk.co.socialcalendar.newsFeed;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import uk.co.socialcalendar.authentication.SessionAttributes;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class NewsFeedController {
+    SessionAttributes sessionAttributes;
+
+    public SessionAttributes getSessionAttributes() {
+        return sessionAttributes;
+    }
+
+    public void setSessionAttributes(SessionAttributes sessionAttributes) {
+        this.sessionAttributes = sessionAttributes;
+    }
+
+
+    @RequestMapping(value = "newsFeed", method = RequestMethod.GET)
+    public ModelAndView showNewsFeed(
+            Model m,
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException, ServletException {
+
+        ModelAndView mav = new ModelAndView("newsFeed");
+        return mav;
+    }
+
 }
--- src/test/java/newsFeed/NewsFeedControllerTest.java
+++ src/test/java/newsFeed/NewsFeedControllerTest.java
@@ -1,4 +1,43 @@
 package newsFeed;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+import testSupport.HttpMocks;
+import uk.co.socialcalendar.newsFeed.NewsFeedController;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class NewsFeedControllerTest {
+    private static final String END_DATE = "2012-01-27 14:15";
+    private static final String START_DATE = "2012-01-25 11:12";
+    private static final String TITLE = "title";
+    private static final String DATE_PATTERN = "yyyy-MM-dd HH:mm";
+    private static final String ME = "me";
+    private static final String MY_NAME = "myName";
+
+    NewsFeedController controller = new NewsFeedController();
+    HttpMocks httpMocks;
+    ModelAndView mav;
+
+    @Before
+    public void setup(){
+        controller = new NewsFeedController();
+        setupHttpMocks();
+    }
+
+    public void setupHttpMocks(){
+        httpMocks = new HttpMocks();
+        controller.setSessionAttributes(httpMocks.getMockSessionAttributes());
+    }
+
+    @Test
+    public void rendersNewsFeedView() throws IOException, ServletException {
+        mav = controller.showNewsFeed(httpMocks.getModel(),
+                httpMocks.getMockHttpServletRequest(), httpMocks.getMockHttpServletResponse());
+        assertEquals("newsFeed",mav.getViewName());
+    }
 }
